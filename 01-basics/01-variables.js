32ba6ba6f0f5de5c6a45b5b4498d0032087b6ada
aashishrana <aashishrana71135@gmail.com>
2023-08-30 10:24:34 -0700

    variables

---
+++ 01-basics/01-variables.js
@@ -0,0 +1,11 @@
+const accountId = 14453;
+let accountEmail = "aashish@google.com"
+var accountPassword = "12345"
+accountCity = "Jaipur"
+let accountState;
+
+
+console.table([accountId , accountEmail , accountPassword , accountCity] , accountState);
+// console.log(accountState);
+
+// prefer not to use var due to block scope and functional scope"
