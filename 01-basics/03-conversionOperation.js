8e153ae7e93c681027d8091c48833cf505223336
aashishrana <aashishrana71135@gmail.com>
2023-08-30 11:07:07 -0700

    Data type conversion

---
+++ 01-basics/03-conversionOperation.js
@@ -0,0 +1,27 @@
+let score = "33";
+let scor2 = "33djfkjs"
+console.log(typeof (score));
+
+let valueInNumber = Number(score);
+console.log(valueInNumber);
+console.log(typeof (valueInNumber));
+
+let valueInNumber2 = Number(scor2);
+console.log(valueInNumber2);
+console.log(typeof valueInNumber);
+console.log(typeof scor2);
+
+
+// "33" => 33
+// "33abc" => NaN
+// true => 1 ; false => 0
+
+let isLoggedIn = 1;
+let BooleanIsLoggedIn = Boolean(isLoggedIn);
+console.log(BooleanIsLoggedIn);
+
+
+let someNumber = 33;
+let stringNumber = String(someNumber);
+console.log(stringNumber);
+console.log(typeof(stringNumber));
