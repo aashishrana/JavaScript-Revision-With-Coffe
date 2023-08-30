39abb1edcf3802b666d02effdd2df3a38c032eda
aashishrana <aashishrana71135@gmail.com>
2023-08-30 10:44:15 -0700

    Data types

---
+++ 01-basics/02-Datatypes.js
@@ -0,0 +1,16 @@
+"use strict"; // treat all js code newer version
+// alert(4 + 4); // we are using nodejs not browser
+
+// Data types
+/*Number,
+BigInt,
+String => " "
+Boolean,
+null => standalone value 
+Symbol => unique */
+
+// Object
+
+console.log(typeof "Aashish");
+console.log(typeof null);
+console.log(typeof (undefined));
