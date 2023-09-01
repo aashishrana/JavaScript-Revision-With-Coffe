ec55ae43c552750b1d286e0405affd671cd60dbd
aashishrana <aashishrana71135@gmail.com>
2023-08-31 17:03:40 -0700

    Operations

--- 01-basics/04-Operation.js
+++ 01-basics/04-Operation.js
@@ -18,12 +18,12 @@ console.log(negValue);
 // console.log(1 + 2 + "2");
 
 
-console.log(+true);
-console.log("");
-console.log(+"");
+// console.log( typeof +true);
+// console.log(typeof "");
+// console.log(typeof +"");
 
 
 
-let num1 , num2 , num3;
-num1 = num2 = num3 = 2 + 2;
+// let num1 , num2 , num3;
+// num1 = num2 = num3 = 2 + 2;
 
