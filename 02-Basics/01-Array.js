0e843c19964e3a46b72368279b8bb6f5079ca2a0
aashishrana <aashishrana71135@gmail.com>
2023-09-04 11:35:11 -0700

    Working with array

--- 02-Basics/01-Array.js
+++ 02-Basics/01-Array.js
@@ -2,5 +2,14 @@ const arr = [1,2,3,,4,5];
 console.log(arr[3]);
 
 const myArr2 = new Array(2,"hello", true, {name : "Ashish"})
-console.log(myArr2[3]);
-console.log(myArr2[3].name);
+// console.log(myArr2[3]);
+// console.log(myArr2[3].name);
+// myArr2.push(6);
+// myArr2.pop();
+
+const newArr = myArr2.join();
+console.log(newArr);
+console.log(typeof myArr2);
+console.log(typeof newArr);
+
+
