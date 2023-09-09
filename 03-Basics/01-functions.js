8783ce4b55a3e373c37e7a2573212b0270e4de7b
aashishrana <aashishrana71135@gmail.com>
2023-09-09 13:42:50 -0700

    Function in javascript

---
+++ 03-Basics/01-functions.js
@@ -0,0 +1,21 @@
+function sayMyName() {
+    console.log("H");
+    console.log("I");
+    console.log("T");
+    console.log("E");
+    console.log("S");
+    console.log("H");
+}
+
+// sayMyName   function refrence
+//sayMyName()  // function execution
+// sayMyName()
+
+function addTwoNumber(number1 , number2) { // parameter
+    console.log(number1 + number2);
+}
+
+addTwoNumber(3 , "a"); // arguments
+addTwoNumber(3 ,5)
+const result = addTwoNumber(5,5);
+console.log("Result is  : ", result );  // output undefined nothing return by the function
