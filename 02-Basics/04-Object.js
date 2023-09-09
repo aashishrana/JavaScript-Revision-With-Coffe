a6f9485c95dc2fa245afd5a2e079a30e06fc084f
aashishrana <aashishrana71135@gmail.com>
2023-09-08 22:28:00 -0700

    Spread operator

--- 02-Basics/04-Object.js
+++ 02-Basics/04-Object.js
@@ -28,7 +28,7 @@ const regularUser = {
 const obj1 = {1: "a", 2: "b"}
 const obj2 = {3 : "c", 4: "d"}
 
-const obj3 = {...obj1 , ...obj2}
+const obj3 = {...obj1 , ...obj2}  // spread operatoor
 console.log(obj3);
 
 console.log(Object.keys(obj1));
