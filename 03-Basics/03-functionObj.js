568ea5aee0c950692e899c5617f9f4c074d31351
aashishrana <aashishrana71135@gmail.com>
2023-09-11 11:26:05 -0700

    passing array and object in function

--- 03-Basics/03-functionObj.js
+++ 03-Basics/03-functionObj.js
@@ -8,5 +8,17 @@ function handleObject(anyObject) {
 
 }
 
-handleObject(user)
-handleObject(user)
+// handleObject(user)
+handleObject({
+    username : "sam",      // in both way you can handle the object by variable and direct
+    price : 399
+})
+
+const myNewArray = [200 , 400 , 100, 600]
+
+function returnSecondValue(getArray) {
+    return getArray[1]
+}
+
+// console.log(returnSecondValue(myNewArray));
+console.log(returnSecondValue([200 , 400 , 500 , 1000]));    // in both way you can handle the array by variable and direct
