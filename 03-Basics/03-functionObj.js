2854c33969bf20731df5039d8c7f8910c7c068b8
aashishrana <aashishrana71135@gmail.com>
2023-09-11 11:15:21 -0700

    Passing object in a function

---
+++ 03-Basics/03-functionObj.js
@@ -0,0 +1,12 @@
+const user = {
+    username : "hitesh",
+    price : 199
+}
+
+function handleObject(anyObject) {
+    console.log((`Username is ${anyObject.username} and price is ${anyObject.price}`));
+
+}
+
+handleObject(user)
+handleObject(user)
