a282a8e44d5cbfe979b39a32e8aa17299e381438
aashishrana <aashishrana71135@gmail.com>
2023-09-11 12:41:17 -0700

    closer type functions

--- 03-Basics/05-closer.js
+++ 03-Basics/05-closer.js
@@ -5,9 +5,23 @@ function one() {
         const website = "youtube"
         console.log(username);
     }
-    console.log(website)
+    // console.log(website)
     two()
 }
 
 one()
+
+
+//////////////************* */
+
+console.log(addOne(5));  // you can access above
+function addOne(num) {
+    return num + 1;
+}
+
+
+addTwo(10)  // can not access above
+const addTwo = function add(num) {  // function expression
+    return num + 2
+}
 
