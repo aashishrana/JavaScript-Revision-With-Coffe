5d9655784599c033aedbbfd57c9d96dee6dc21b0
aashishrana <aashishrana71135@gmail.com>
2023-09-18 22:39:49 -0700

    switch case

---
+++ 04-controlFlow/switch.js
@@ -0,0 +1,29 @@
+const month = 3
+switch (month) {
+    case 1:
+        console.log("January");
+        break;
+
+    case 2:
+        console.log("February"); 
+        break;  
+        
+    case 3:
+        console.log("March");       
+        
+    case 5:
+        console.log("March");     //Break keyword is used to terminate the loop if no break used all case execute except default
+        break;
+    case 3:
+        console.log("March");       
+        break;
+    case 3:
+        console.log("March");       
+        break;
+
+    case 4:
+        console.log("April");    
+
+    default:  //default case execute if no case match
+        break;
+}
