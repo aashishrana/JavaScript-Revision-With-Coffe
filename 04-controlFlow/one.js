72e4fec537d27c4a0d934f019d18356a3ac7a857
aashishrana <aashishrana71135@gmail.com>
2023-09-18 22:03:59 -0700

    Condition check

--- 04-controlFlow/one.js
+++ 04-controlFlow/one.js
@@ -1,5 +1,15 @@
 //if
+const balance = 1000
 
-if(true) {
-    
+if(balance < 500) console.log("test")
+
+const userLoggedIn = true
+const debitCard = true
+
+
+
+if(userLoggedIn && debitCard && 2==2) {
+    console.log("Allow to buy course");
 }
+
+
