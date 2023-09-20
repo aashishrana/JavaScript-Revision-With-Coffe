41f0dea3ea360ee58ca15a863a22e8a5e91919a0
aashishrana <aashishrana71135@gmail.com>
2023-09-20 11:50:07 -0700

    for of loop

---
+++ 05-iteration/03-for-of.js
@@ -0,0 +1,47 @@
+// for of
+
+// ["", "", ""]
+// [{}, {}, {}]
+
+
+const arr = [1,2,3,4,5]
+
+for (const num of arr) {
+    // console.log(num);
+    
+}
+
+const greeting = "Hello WOrld"
+for (const greet of greeting) {
+    // console.log(`Each character is ${greet}`);
+}
+
+// Maps  stores unique values 
+
+const map = new Map()
+map.set("IN", "India")
+map.set("USA", "United State Of America")
+// console.log(map);
+
+for (const key of map) {
+    // console.log(key);  This will print all values
+}
+
+
+for (const [key, value] of map) {
+    console.log(key, ":-", value);
+    
+}
+
+
+//Object
+const myObject = {
+    sirname : "Kushwaha",
+    name : "Aashish"
+
+}
+
+// for (const key of myObject) {
+//     console.log(key);       for off loop not works on Objects
+    
+// }
