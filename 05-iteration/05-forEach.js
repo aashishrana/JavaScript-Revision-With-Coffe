5c44bb510c38e52ad2e6f0395df70516925fe1d3
aashishrana <aashishrana71135@gmail.com>
2023-09-20 12:27:23 -0700

    for each loop

---
+++ 05-iteration/05-forEach.js
@@ -0,0 +1,38 @@
+const coding = ["c++", "java", "pyhton"]
+
+// coding.forEach(function (item)  {  // pass function
+//     console.log(item);
+
+
+// }) 
+
+// coding.forEach((value)=>{
+//     console.log(value);
+
+// })
+
+// coding.forEach(printMe)
+
+function printMe() {
+    console.log("Hello");
+}
+
+// coding.forEach( (item , index, arr) => {
+//     console.log(item , index , arr);
+// })
+
+const myCoding = [
+    {
+        lang : "js"
+
+    },
+    {
+
+    },
+    {
+
+    }
+]
+myCoding.forEach((item) => {
+    console.log(item.lang);
+})
