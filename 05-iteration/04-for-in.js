9eca625a48454e25444573c034eea33cde3a7829
aashishrana <aashishrana71135@gmail.com>
2023-09-20 12:26:34 -0700

    for in loop

---
+++ 05-iteration/04-for-in.js
@@ -0,0 +1,25 @@
+const myObject = {
+    js : "js for javaScript",
+    cpp : "for c++",
+    swift : "Swift by apple"
+}
+
+
+// for (const key in myObject) {
+//     console.log(key);
+// }
+
+// for (const key in myObject) {
+//     console.log(`${key} shortcut is for ${myObject[key]}`);
+//     console.log(myObject[key]);
+// }
+
+
+//Arraays
+
+const arr = [1,2,3,45,6]
+
+for (const key in arr) {
+    console.log(key);
+    // console.log(arr[key]);
+}
