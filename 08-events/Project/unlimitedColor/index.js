26d441684f17c3c4b38b417359a374e39e2b54d1
aashishrana <aashishrana71135@gmail.com>
2023-09-29 10:56:55 -0700

    Background color switcher

---
+++ 08-events/Project/unlimitedColor/index.js
@@ -0,0 +1,43 @@
+//genrate a random color 
+
+const randomColor = function() {
+    const hex = "0123456789ACDEF"
+    let color = '#'
+    for(let i = 0; i < 6; i++) {
+        color += hex[Math.floor(Math.random()*16)];
+
+    }
+    return color;
+};
+
+// console.log(randomColor());
+
+let intervalId;
+
+const startChangingColor = function() {
+    if(intervalId == null) {
+        intervalId = setInterval(changeBgColor, 1000);
+
+    }
+     
+    
+
+    function changeBgColor() {
+        document.body.style.backgroundColor = randomColor();
+
+    }
+
+}
+
+const stopChangingColor = function() {
+    clearInterval(intervalId);
+    intervalId = null;
+
+}
+document.querySelector("#start").addEventListener("click", startChangingColor)
+document.querySelector("#stop").addEventListener("click", stopChangingColor)
+
+
+// How to genrate random values Math.random() return valued between Zero and One
+// console.log (Math.floor(Math.random()*10));// THis will return between 0 and 10 but we need 16
+// console.log (Math.floor(Math.random()*16));
