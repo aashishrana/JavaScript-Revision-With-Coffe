ce5d6de92e8ca946cce8f40ca8ef59b1f0d78d2f
Aashish rana <aashishrana71135@gmail.com>
2024-02-01 18:55:29 +0530

    operation revision

--- 01-basics/04-Operation.js
+++ 01-basics/04-Operation.js
@@ -4,23 +4,23 @@ let value = 3
 let negValue = -value
 console.log(negValue);
 
-// console.log(2 + 2);
-// console.log(2 -2);
+console.log(2 + 2);
+console.log(2 -2);
 // console.log(2 * 2);
 // console.log(2 ** 3);
-// console.log(2/3);
-// console.log(2 % 3);
+console.log(2/3);
+console.log(2 % 3);
 
 
 // console.log("1" + 2);
 // console.log(1 + "2");
-// console.log("1" + 2 + 2);
-// console.log(1 + 2 + "2");
+console.log("1" + 2 + 2);
+console.log(1 + 2 + "2");
 
 
-// console.log( typeof +true);
-// console.log(typeof "");
-// console.log(typeof +"");
+console.log( typeof +true);
+console.log(typeof "");
+console.log(typeof +"");
 
 
 
