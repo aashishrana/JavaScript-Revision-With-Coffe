c58fe7159a4b4f732de319ab1d9b8384e7a4c96e
Aashish rana <aashishrana71135@gmail.com>
2024-02-01 18:54:57 +0530

    conversion operation revision

--- 01-basics/03-conversionOperation.js
+++ 01-basics/03-conversionOperation.js
@@ -1,14 +1,17 @@
 let score = "33";
 let scor2 = "33djfkjs"
-console.log(typeof (score));
+// console.log(typeof (score));
 
 let valueInNumber = Number(score); // type casting
-console.log(valueInNumber);
-console.log(typeof (valueInNumber));
+// console.log(valueInNumber);
+
+
+// console.log(typeof (valueInNumber));
 
 let valueInNumber2 = Number(scor2);
-console.log(valueInNumber2);
-console.log(typeof valueInNumber);
+// console.log(valueInNumber2);
+// console.log(typeof valueInNumber);
+
 console.log(typeof scor2);
 
 
@@ -18,13 +21,13 @@ console.log(typeof scor2);
 
 let isLoggedIn = 1;
 let BooleanIsLoggedIn = Boolean(isLoggedIn);
-console.log(BooleanIsLoggedIn);
+// console.log(BooleanIsLoggedIn);
 
 
 let someNumber = 33;
 let stringNumber = String(someNumber);
-console.log(stringNumber);
-console.log(typeof(stringNumber));
+// console.log(stringNumber);
+// console.log(typeof(stringNumber));
 
 
 //           /************* OPERATION ************/
