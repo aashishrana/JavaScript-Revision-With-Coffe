e8245638cfc9189121d485cedfc2a7a321cd6ed6
Aashish rana <aashishrana71135@gmail.com>
2024-02-01 18:53:10 +0530

    function revision

--- 03-functionBasic/02-function.js
+++ 03-functionBasic/02-function.js
@@ -2,7 +2,7 @@ function calculateCartPrice(...num1) {
     return num1
 }
 
-// console.log(calculateCartPrice(200, 400, 500));
+console.log(calculateCartPrice(200, 400, 500));
 
 function newfunction(value1 , value2 , ...num1) {
     return num1
