a24416becf334bc010ff1b095924d62ff6aa05b6
Aashish rana <aashishrana71135@gmail.com>
2024-02-01 18:57:19 +0530

    Destructuring  revision

--- 02-Basics/05-Destructuring.js
+++ 02-Basics/05-Destructuring.js
@@ -4,9 +4,9 @@ const course = {
     courseInstructor : "hitesh"
 }
 
-// console.log(course["price"])
-// console.log(course.price)
+console.log(course["price"])
+console.log(course.price)
 
 const {courseInstructor : instructor} = course
-// console.log(courseInstructor);
+// console.log("courseInstructor");
 console.log(instructor)
