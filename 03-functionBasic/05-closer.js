38287bba20e0a429a52ad08c3191a25e1b570505
Aashish rana <aashishrana71135@gmail.com>
2024-02-01 18:53:59 +0530

    function revision

--- 03-functionBasic/05-closer.js
+++ 03-functionBasic/05-closer.js
@@ -1,5 +1,6 @@
 function one() {
     const username = "hitesh"
+    console.log("one");
 
     function two() {
         const website = "youtube"
@@ -14,14 +15,14 @@ one()
 
 //////////////************* */
 
-console.log(addOne(5));  // you can access above
-function addOne(num) {
-    return num + 1;
-}
+// console.log(addOne(5));  // you can access above
+// function addOne(num) {
+//     return num + 1;
+// }
 
 
-addTwo(10)  // can not access above
-const addTwo = function add(num) {  // function expression
-    return num + 2
-}
+// addTwo(10)  // can not access above
+// const addTwo = function add(num) {  // function expression
+//     return num + 2
+// }
 
